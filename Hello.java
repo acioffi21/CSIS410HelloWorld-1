099e3af7141ce7c53d3192a02261eda693afe381
serenemedina <se27medi@siena.edu>
2017-10-10 15:58:11 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Hello here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Hello
+{
+    public static void main()
+    {
+        System.out.println("Hello, world!");
+    }
+}
